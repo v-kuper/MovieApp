9382429d1a1a78dd4916b29f84a892c2e1406901
v-kuper <vkuper7@gmail.com>
2025-02-19 00:52:29 +0300

    feat: add LazyColumn

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.movieapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -14,6 +31,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,18 +64,62 @@ fun MyApp(content: @Composable () -> Unit) {
                 )
             }
         ) { innerPadding ->
-            MainContent(modifier = Modifier.padding(innerPadding))
+            Surface(
+                modifier = Modifier.padding(innerPadding),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                MainContent()
+            }
         }
     }
 }
 
 @Composable
-fun MainContent(modifier: Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
+fun MainContent(movieList: List<String> = listOf(
+    "Avatar",
+    "300",
+    "Harry Potter",
+    "Life",
+)) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            LazyColumn {
+                items(items = movieList) {
+                    MovieRow(movie = it) { movie ->
+
+                    }
+                }
+            }
+        }
+}
+
+@Composable
+fun MovieRow(movie: String = "", onItemClick: (movie: String) -> Unit = {}) {
+    Card(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth()
+        .height(130.dp)
+        .clickable {
+            onItemClick(movie)
+        },
+        shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+        elevation  = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
     ) {
-        Text(text = "Hello!", modifier = Modifier.padding(16.dp))
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            ) {
+                Surface(modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                    color = Color.White
+                ) {
+                    Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+                }
+                Text(text = movie)
+        }
     }
 }
 
