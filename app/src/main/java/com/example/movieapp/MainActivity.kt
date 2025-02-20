42b61e544d91f0cbd9edc7f37104a19ed3ae78d2
v-kuper <vkuper7@gmail.com>
2025-02-21 00:25:56 +0300

    feat: add screens navigation

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -4,37 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.movieapp.navigation.MovieNavigation
 import com.example.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,88 +14,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApp {}
+            MyApp {
+                MovieNavigation()
+            }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     MovieAppTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            topBar = {
-                TopAppBar(
-                    title = { Text(text = "Movies") },
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = Color.Magenta,
-                        titleContentColor = Color.White
-                    )
-                )
-            }
-        ) { innerPadding ->
-            Surface(
-                modifier = Modifier.padding(innerPadding),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                MainContent()
-            }
-        }
-    }
-}
-
-@Composable
-fun MainContent(movieList: List<String> = listOf(
-    "Avatar",
-    "300",
-    "Harry Potter",
-    "Life",
-)) {
-        Column(modifier = Modifier.padding(12.dp)) {
-            LazyColumn {
-                items(items = movieList) {
-                    MovieRow(movie = it) { movie ->
-
-                    }
-                }
-            }
-        }
-}
-
-@Composable
-fun MovieRow(movie: String = "", onItemClick: (movie: String) -> Unit = {}) {
-    Card(modifier = Modifier
-        .padding(4.dp)
-        .fillMaxWidth()
-        .height(130.dp)
-        .clickable {
-            onItemClick(movie)
-        },
-        shape = RoundedCornerShape(corner = CornerSize(12.dp)),
-        elevation  = CardDefaults.cardElevation(4.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White
-        ),
-    ) {
-        Row (
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
-            ) {
-                Surface(modifier = Modifier
-                    .padding(12.dp)
-                    .size(100.dp),
-                    color = Color.White
-                ) {
-                    Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
-                }
-                Text(text = movie)
-        }
+        content()
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApp {}
+fun DefaultPreview() {
+    MyApp {
+        MovieNavigation()
+    }
 }
--- app/src/main/java/com/example/movieapp/navigation/MovieNovigation.kt
+++ app/src/main/java/com/example/movieapp/navigation/MovieNovigation.kt
@@ -4,15 +4,21 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.movieapp.screens.home.HomeScreen
+import com.example.movieapp.screens.details.DetailsScreen
 
 @Composable
 fun MovieNavigation() {
-    val navControl = rememberNavController()
+    val navController = rememberNavController()
     NavHost(
-        navController = navControl,
+        navController = navController,
         startDestination = MovieScreens.HomeScreen.name) {
         composable(MovieScreens.HomeScreen.name) {
-//            HomeScreen()
+            HomeScreen(navController = navController)
+        }
+
+        composable(MovieScreens.DetailsScreen.name) {
+            DetailsScreen(navController = navController)
         }
     }
 }
